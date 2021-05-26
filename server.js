2531af500fe8e7421139249b12dd481e794f4ea9
AustenHsiao <Austenhsiao@gmail.com>
2021-05-26 13:10:33 -0700

    logs in console and handles duplicate names

--- server.js
+++ server.js
@@ -7,15 +7,39 @@ const io = new Server(server);
 const port = process.env.PORT || 8080;
 
 
+var users = {}; //store usernames
+var rooms = new Set(); //store roomnames
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('somebody connected');
 
     socket.on('sendMsg', (messageObject) => {
-        console.log(messageObject);
+        io.sockets.emit('chat', messageObject);
+    });
+
+    socket.on('urChange', (data) => {
+        var flag = 1;
+        var name = data.uname.toLowerCase();
+        if(users[name] !== undefined){ // key exists
+            socket.emit('urResponse', 0);
+        }else{
+            users[name] = name
+            socket.emit('urResponse', 1);
+            flag = 0;
+        }
+
+        if(flag === 0){
+            var room = data.rname.toLowerCase();
+            rooms.add(room);
+            console.log(`${data.oldName} changed name to ${data.uname} and moved from ${data.oldRoom} to ${room}`);
+
+        }
+        
+
     });
 });
 
