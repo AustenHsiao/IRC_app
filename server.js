c0c4d930f77038d5ef1c176065e0cfe734d4e340
AustenHsiao <Austenhsiao@gmail.com>
2021-05-30 11:58:53 -0700

    changed functionality w username

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 
 
 var users = {}; //store usernames
-var rooms = new Set(); //store roomnames
+var rooms = {}; //store roomnames
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -21,25 +21,48 @@ io.on('connection', (socket) => {
         io.sockets.emit('chat', messageObject);
     });
 
-    socket.on('urChange', (data) => {
-        var flag = 1;
-        var name = data.uname.toLowerCase();
-        if(users[name] !== undefined){ // key exists
-            socket.emit('urResponse', 0);
+    socket.on('set_username', (data) => {
+        var newUser = data.user.toLowerCase();
+        if(users[newUser] !== undefined){
+            socket.emit('set_username_response', 0);
         }else{
-            users[name] = name
-            socket.emit('urResponse', 1);
-            flag = 0;
+            users[newUser] = {"name":data.user};
+            socket.emit('set_username_response', data.user);
+            console.log(users);
+        } 
+    });
+    
+    socket.on("refreshList", data => {
+        socket.emit('refresh_room_list', rooms);
+    });
+
+    socket.on('create_chatroom', (data) => {
+        var newRoom = data.room.toLowerCase();
+        var username = data.name.toLowerCase();
+        if(rooms[newRoom] !== undefined){
+            socket.emit('create_chatroom_response', 0);
+        }else{
+            users[username] = data; // update userdata to include a room
+            rooms[newRoom] = 1; 
+            socket.emit('create_chatroom_response', data.room);
+            console.log(users);
         }
+        // in any case, send the rooms list back
+        socket.emit('refresh_room_list', rooms);
+    });
 
-        if(flag === 0){
-            var room = data.rname.toLowerCase();
-            rooms.add(room);
-            console.log(`${data.oldName} changed name to ${data.uname} and moved from ${data.oldRoom} to ${room}`);
+    socket.on('join_chatroom', (data) => {
+        var joinRoom = data.room.toLowerCase();
+        var username = data.name.toLowerCase();
 
+        if(rooms[joinRoom] === undefined){
+            socket.emit('join_chatroom_response', 0);
+        }else{
+            users[username] = data;
+            socket.emit('join_chatroom_response', data.room);
+            console.log(users);
         }
-        
-
+        socket.emit('refresh_room_list', rooms);
     });
 });
 
